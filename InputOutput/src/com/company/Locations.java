5e46b9a1818d55bb10aa0c90cb788e14a98cbebb
emrecoskun705 <emrecoskun705@gmail.com>
2020-03-01 01:47:43 +0300

    byte streams

--- InputOutput/src/com/company/Locations.java
+++ InputOutput/src/com/company/Locations.java
@@ -7,34 +7,25 @@ public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<>();
 
     public static void main(String[] args) throws IOException {
-        try(BufferedWriter locFile = new BufferedWriter(new FileWriter("locations.txt"));
-            BufferedWriter dirFile = new BufferedWriter(new FileWriter("directions.txt"))) {
+        try(DataOutputStream locFile = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("locations.dat")))) {
             for(Location location : locations.values()) {
-                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+                locFile.writeInt(location.getLocationID());
+                locFile.writeUTF(location.getDescription());
+                System.out.println("Writing location " + location.getLocationID() + " : " + location.getDescription());
+                System.out.println("Writing " + (location.getExits().size() - 1) + " exits");
+                locFile.writeInt(location.getExits().size() - 1);
                 for(String direction : location.getExits().keySet()) {
                     if(!direction.equalsIgnoreCase("Q")) {
-                        dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
+                        System.out.println("\t\t" + direction + "," + location.getExits().get(direction));
+                        locFile.writeUTF(direction);
+                        locFile.writeInt(location.getExits().get(direction));
                     }
-
                 }
             }
+
         }
 
 
-//        FileWriter locFile = null;
-//        try {
-//            locFile = new FileWriter("locations.txt");
-//            for(Location location : locations.values()) {
-//                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
-//            }
-//        } finally {
-//            System.out.println("In finally block");
-//            if(locFile != null) {
-//                System.out.println("Attempting to close locFile");
-//                locFile.close();
-//            }
-//
-//        }
     }
 
     static {
